d3ff3c8cb19ee50c829c03322ddf03ea770059c0
miyayuu06 <yunafermi@gmail.com>
2025-09-10 21:00:11 +0200

    Introduced dark mode

--- prueba/navi.php
+++ prueba/navi.php
@@ -1,3 +1,25 @@
 <ul>
   <li><a href="pagina.php">Projects</a></li>
+  <li style="float:right;">
+    <button id="theme-toggle" class="nav-btn" onclick="toggleTheme()">
+      <i id="theme-icon" class="fas fa-moon"></i>
+    </button>
+  </li>
 </ul>
+
+<script>
+function toggleTheme() {
+  const body = document.body;
+  const themeIcon = document.getElementById("theme-icon");
+
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+  } else {
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+  }
+}
+</script>
---
+++ prueba/theme.js
@@ -0,0 +1,6 @@
+<script>
+function toggleTheme() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
